a79f5f489131c54d8a556bd7d46ffdff0c522a8b
root <root@danzxwanzofc.site>
2026-01-06 16:23:25 +0000

    Fix backend db connection and chat 401

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: true // Ini akan membuat server bisa diakses secara publik (0.0.0.0)
-  }
 })
